3bb93847685873a89218e09ff3c51376b4f6d1e8
Jose Cruz <josec300@live.com>
2020-07-14 14:16:35 -0400

    Finsh assignments, has problem for numbers over 100

--- src/main/java/com/github/perscholas/GradeEvaluator.java
+++ src/main/java/com/github/perscholas/GradeEvaluator.java
@@ -5,26 +5,74 @@ package com.github.perscholas;
  */
 public class GradeEvaluator {
     public Boolean isGradeA(Double score) {
-        return null;
+        return score >= 90 && score <= 100;
     }
 
     public Boolean isGradeB(Double score) {
-        return null;
+        if(score>=80 && score<=89)
+        {
+            return true;
+        }
+        else {
+            return  false;
+        }
     }
 
     public Boolean isGradeC(Double score) {
-        return null;
+       if (score>=70 && score<=79)
+       {
+           return true;
+       }
+       else {
+           return false;
+       }
     }
 
     public Boolean isGradeD(Double score) {
-        return null;
+        if(score<=69 && score>=65 )
+        {
+            return  true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public Boolean isGradeF(Double score) {
-        return null;
+        if(score<=64)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public Character getGrade(Double score) {
-        return null;
+
+        if (isGradeA(score)==true)
+        {
+    return 'A';
+        }
+        else if(isGradeB(score)==true)
+        {
+            return 'B';
+        }
+        else if(isGradeC(score)==true)
+        {
+            return 'C';
+        }
+        else if(isGradeD(score)==true)
+        {
+            return 'D';
+        }
+        else if (isGradeF(score)==true)
+        {
+            return 'F';
+        }
+        else {
+            return null;
+        }
     }
 }
--- src/main/java/com/github/perscholas/MainApplication.java
+++ src/main/java/com/github/perscholas/MainApplication.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 /**
  * Created by leon on 7/14/2020.
+ * This file is just the main application and just returns the output of the work done in gradeEvaluator
  */
 public class MainApplication {
     public static void main(String[] args) {
         do {
             GradeEvaluator gradeEvaluator = new GradeEvaluator();
-            Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);   // this calls make a new function for scanner
             System.out.println("What score did the student receive in the class?");
             String studentScoreAsString = scanner.nextLine();
             Double studentScoreAsDouble = Double.parseDouble(studentScoreAsString);
